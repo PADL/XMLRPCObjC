5a28a558ef538689da21d9f1ef0aee442a174797
Luke Howard <lukeh@padl.com>
2001-02-10 17:18:25 +0000

    Updated stuff

---
+++ XMLRPCTestServer/main.m
@@ -0,0 +1,29 @@
+#import <Foundation/Foundation.h>
+#import <XMLRPCObjC/XMLRPCObjC.h>
+
+@interface sample : NSObject
+- (NSNumber *)add:(NSNumber *)x :(NSNumber *)y;
+@end
+
+@implementation sample
+- (NSNumber *)add:(NSNumber *)x :(NSNumber *)y
+{
+	int result;
+	
+	result = [x intValue] + [y intValue];
+	
+	return [NSNumber numberWithInt:result];
+}
+@end
+
+int main (int argc, const char *argv[]) {
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	XMLRPCServer *server = [XMLRPCServer server];
+
+	[server setObjectAutoCreation:YES];
+	[server run];
+
+	[pool release];
+	exit(0);
+}
+
